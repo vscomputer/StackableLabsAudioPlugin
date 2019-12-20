654aa75835681b142e8214dfed3aa02727a89a7f
Clint Hoagland <bachelor.one@gmail.com>
2019-12-19 21:20:01 -0500

    + pi values

--- StackableLabsAudioPlugin/Source/SLAPAudioHelpers.h
+++ StackableLabsAudioPlugin/Source/SLAPAudioHelpers.h
@@ -10,6 +10,9 @@
 
 #pragma once
 
+const static double sPi = 3.14159265;
+const static double sTwoPi = 6.28318531;
+
 inline float slap_linear_interp(float v0, float v1, float t)
 {
 	return (1 - t) * v0 + t * v1;
