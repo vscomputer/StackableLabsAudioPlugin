3c5e95ca95b3d92fbe3ea9f281048a8453fa09b9
Clint Hoagland <bachelor.one@gmail.com>
2019-12-25 15:40:59 -0500

    + interface heights and widths

--- StackableLabsAudioPlugin/Source/SLAPInterfaceDefines.h
+++ StackableLabsAudioPlugin/Source/SLAPInterfaceDefines.h
@@ -9,3 +9,20 @@
 */
 
 #pragma once
+const static int MAIN_PANEL_WIDTH = 700;
+const static int MAIN_PANEL_HEIGHT = 300;
+
+const static int TOP_PANEL_WIDTH = MAIN_PANEL_WIDTH;
+const static int TOP_PANEL_HEIGHT = 45;
+
+const static int GAIN_PANEL_WIDTH = 100;
+const static int GAIN_PANEL_HEIGHT = MAIN_PANEL_HEIGHT - TOP_PANEL_HEIGHT;
+
+const static int CENTER_PANEL_WIDTH = MAIN_PANEL_WIDTH - (GAIN_PANEL_WIDTH * 2);
+const static int CENTER_PANEL_HEIGHT = GAIN_PANEL_HEIGHT;
+
+const static int CENTER_PANEL_MENU_BAR_WIDTH = CENTER_PANEL_WIDTH;
+const static int CENTER_PANEL_MENU_BAR_HEIGHT = 35;
+
+const static int FX_PANEL_WIDTH = CENTER_PANEL_WIDTH;
+const static int FX_PANEL_HEIGHT = CENTER_PANEL_HEIGHT - CENTER_PANEL_MENU_BAR_HEIGHT;
