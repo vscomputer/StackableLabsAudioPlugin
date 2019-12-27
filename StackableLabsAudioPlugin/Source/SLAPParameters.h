19de418f9ed8ad0a7cd64cb6ae6bca1dd1a1f413
Clint Hoagland <bachelor.one@gmail.com>
2019-12-26 21:26:33 -0500

    fill in parameters enum

--- StackableLabsAudioPlugin/Source/SLAPParameters.h
+++ StackableLabsAudioPlugin/Source/SLAPParameters.h
@@ -9,3 +9,16 @@
 */
 
 #pragma once
+
+enum kSLAPParameter
+{
+	kParameter_InputGain = 0,
+	kParameter_DelayTime,
+	kParameter_DelayFeedback,
+	kParameter_DelayWetDry,
+	kParameter_DelayType,
+	kParameter_OutputGain,
+	kParameter_ModulationRate,
+	kParameter_ModulationDepth,
+	kParameter_TotalNumParameters
+};
