f44270ac2e1c70f1fdc42cf51311b87c232166f3
Clint Hoagland <bachelor.one@gmail.com>
2020-01-04 22:15:25 -0500

    +paintComponentLabel method

--- StackableLabsAudioPlugin/Source/SLAPHelperFunctions.h
+++ StackableLabsAudioPlugin/Source/SLAPHelperFunctions.h
@@ -9,3 +9,19 @@
 */
 
 #pragma once
+#include "JuceHeader.h"
+
+inline void paintComponentLabel(Graphics& g, Component* inComponent)
+{
+	const int x = inComponent->getX();
+	const int y = inComponent->getY();
+	const int w = inComponent->getWidth();
+	const int h = 20;
+
+	const String label = inComponent->getName();
+
+	const float cornerSize = 3.0f;
+
+	g.setColour(Colours::darkgrey);
+	g.drawFittedText(label, x, y, w, h, Justification::centred, 1);
+}
