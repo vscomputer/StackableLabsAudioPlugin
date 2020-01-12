de681fe051cfd542064f4414fc6890c5317c0d73
Clint Hoagland <bachelor.one@gmail.com>
2020-01-12 17:26:45 -0500

    Now allowing 120% delay amount w soft clipping
    This allows for some wonderfully crunchy sounds when you modulate other values

--- StackableLabsAudioPlugin/Source/SLAPDelay.cpp
+++ StackableLabsAudioPlugin/Source/SLAPDelay.cpp
@@ -37,9 +37,7 @@ void SLAPDelay::process(float* inAudio, float inTime, float inFeedback, float in
 {	
 	const float wet = inWetDry;
 	const float dry = 1.0 - wet;
-	const float feedbackMapped = jmap(inFeedback, 0.0f, 1.0f, 0.0f, 0.95f);
-
-	
+	const float feedbackMapped = jmap(inFeedback, 0.0f, 1.0f, 0.0f, 1.2f);	
 
 	for (int i = 0; i < inNumSamplesToRender; i++)
 	{
