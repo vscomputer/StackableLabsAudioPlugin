0a1d7c70cae348c0da9278ca5c6cdf109a06b67b
Clint Hoagland <bachelor.one@gmail.com>
2020-01-24 15:40:26 -0500

    start to override LookAndFeel
    committed at the end of module 4.2

--- StackableLabsAudioPlugin/Source/SLAPLookAndFeel.h
+++ StackableLabsAudioPlugin/Source/SLAPLookAndFeel.h
@@ -9,3 +9,51 @@
 */
 
 #pragma once
+#include "JuceHeader.h"
+#include "SLAPInterfaceDefines.h"
+
+class SLAPLookAndFeel
+	: public LookAndFeel_V4
+{
+public:
+	SLAPLookAndFeel()
+	{
+		setColour(ComboBox::backgroundColourId, SLAPColour_3);
+		setColour(ComboBox::outlineColourId, SLAPColour_2);
+		setColour(ComboBox::arrowColourId, SLAPColour_1);
+		setColour(ComboBox::textColourId, SLAPColour_1);
+
+		setColour(TextButton::buttonColourId, SLAPColour_1);
+		setColour(TextButton::textColourOnId, SLAPColour_1);
+		setColour(TextButton::textColourOffId, SLAPColour_1);
+	}
+	~SLAPLookAndFeel();
+
+	Font getTextButtonFont(TextButton&, int buttonHeight) override
+	{
+		return font_1;
+	}
+
+	void drawButtonBackground(Graphics& g, Button& button, const Colour& backgroundColour,
+		bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+	{
+		Colour fillColor;
+
+		if(shouldDrawButtonAsDown)
+		{
+			fillColor = SLAPColour_5;
+		}
+		else if(shouldDrawButtonAsHighlighted)
+		{
+			fillColor = SLAPColour_3;
+		}
+
+		const float cornerSize = 6.0f;
+		const juce::Rectangle<float> bounds = button.getLocalBounds().toFloat().reduced(0.5f, 0.5f);
+
+		g.setColour(fillColor);
+		g.fillRoundedRectangle(bounds.reduced(1), cornerSize);
+
+	}
+private:
+};
