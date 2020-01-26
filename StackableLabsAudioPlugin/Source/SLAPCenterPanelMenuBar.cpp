f3c5da1df7904fb595b636a088fdff4f1c818765
Clint Hoagland <bachelor.one@gmail.com>
2020-01-26 15:20:15 -0500

    CenterPanelMenuBar uses unique_ptrs

--- StackableLabsAudioPlugin/Source/SLAPCenterPanelMenuBar.cpp
+++ StackableLabsAudioPlugin/Source/SLAPCenterPanelMenuBar.cpp
@@ -17,13 +17,12 @@ SLAPCenterPanelMenuBar::SLAPCenterPanelMenuBar(StackableLabsAudioPluginAudioProc
 	setSize(CENTER_PANEL_MENU_BAR_WIDTH, CENTER_PANEL_MENU_BAR_HEIGHT);
 
 	const int width = 85;
-
-	_fxTypeComboBox = new SLAPParameterComboBox(_processor->parameters, SLAPParameterId[kParameter_DelayType]);
+	_fxTypeComboBox = std::make_unique<SLAPParameterComboBox>(_processor->parameters, SLAPParameterId[kParameter_DelayType]);
 	_fxTypeComboBox->setBounds(getWidth() - width, 0, width, getHeight());
 	_fxTypeComboBox->addItem("DELAY", 1);
 	_fxTypeComboBox->addItem("CHORUS", 2);
 	_fxTypeComboBox->setSelectedItemIndex((int)_processor->getParameter(kParameter_DelayType), dontSendNotification);
-	addAndMakeVisible(_fxTypeComboBox);
+	addAndMakeVisible(_fxTypeComboBox.get());
 }
 
 SLAPCenterPanelMenuBar::~SLAPCenterPanelMenuBar()
