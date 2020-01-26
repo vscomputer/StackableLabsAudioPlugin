884158ced024b90928dab1e045f6ea7a99414528
Clint Hoagland <bachelor.one@gmail.com>
2020-01-26 15:43:48 -0500

    MainPanel uses unique_ptrs

--- StackableLabsAudioPlugin/Source/SLAPMainPanel.cpp
+++ StackableLabsAudioPlugin/Source/SLAPMainPanel.cpp
@@ -15,23 +15,23 @@ SLAPMainPanel::SLAPMainPanel(StackableLabsAudioPluginAudioProcessor* inProcessor
 	: SLAPPanelBase(inProcessor)
 {
 	setSize(MAIN_PANEL_WIDTH, MAIN_PANEL_WIDTH);
-	_topPanel = new SLAPTopPanel(inProcessor);
+	_topPanel = std::make_unique<SLAPTopPanel>(inProcessor);
 	_topPanel->setTopLeftPosition(0, 0);
-	addAndMakeVisible(_topPanel);
+	addAndMakeVisible(_topPanel.get());
 
-	_inputGainPanel = new SLAPGainPanel(inProcessor);
+	_inputGainPanel = std::make_unique<SLAPGainPanel>(inProcessor);
 	_inputGainPanel->setTopLeftPosition(0, TOP_PANEL_HEIGHT);
 	_inputGainPanel->setParameterId(kParameter_InputGain);
-	addAndMakeVisible(_inputGainPanel);
+	addAndMakeVisible(_inputGainPanel.get());
 
-	_outputGainPanel = new SLAPGainPanel(inProcessor);
+	_outputGainPanel = std::make_unique<SLAPGainPanel>(inProcessor);
 	_outputGainPanel->setTopLeftPosition(MAIN_PANEL_WIDTH - GAIN_PANEL_WIDTH, TOP_PANEL_HEIGHT);
 	_outputGainPanel->setParameterId(kParameter_OutputGain);
-	addAndMakeVisible(_outputGainPanel);
+	addAndMakeVisible(_outputGainPanel.get());
 
-	_centerPanel = new SLAPCenterPanel(inProcessor);
+	_centerPanel = std::make_unique<SLAPCenterPanel>(inProcessor);
 	_centerPanel->setTopLeftPosition(GAIN_PANEL_WIDTH, TOP_PANEL_HEIGHT);
-	addAndMakeVisible(_centerPanel);
+	addAndMakeVisible(_centerPanel.get());
 
 }
 
